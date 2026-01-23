c77f06462255cd7e84f16d0f1dc10bc4004eb19f
Signature Dubai <wzm2383983461@163.com>
2026-01-23 16:45:28 +0800

    Rename image files: change 'image copy X.png' to 'image-copy-X.png' in images_new directory

---
+++ rename-images-final.ps1
@@ -0,0 +1,18 @@
+$imagesNewPath = "e:\工作\迪拜旗舰房产\duby\duby\public\images_new"
+
+if (Test-Path $imagesNewPath) {
+    Get-ChildItem -Path $imagesNewPath -Recurse -File | ForEach-Object {
+        $oldName = $_.Name
+        if ($oldName -match '^image copy (\d+)\.png$') {
+            $newName = "image-copy-$($matches[1]).png"
+            if ($oldName -ne $newName) {
+                $newPath = Join-Path $_.DirectoryName $newName
+                Rename-Item $_.FullName -NewName $newName
+                Write-Host "Renamed: $oldName -> $newName"
+            }
+        }
+    }
+    Write-Host "Done renaming files"
+} else {
+    Write-Host "images_new directory not found"
+}
---
+++ rename-images-simple.ps1
@@ -0,0 +1,15 @@
+$imagesNewPath = "e:\工作\迪拜旗舰房产\duby\duby\public\images_new"
+
+Get-ChildItem -Path $imagesNewPath -Recurse -File | ForEach-Object {
+    $oldName = $_.Name
+    if ($oldName -match '^image copy (\d+)\.png$') {
+        $newName = "image-copy-$($matches[1]).png"
+        if ($oldName -ne $newName) {
+            $newPath = Join-Path $_.DirectoryName $newName
+            Rename-Item $_.FullName -NewName $newName
+            Write-Host "Renamed: $oldName -> $newName"
+        }
+    }
+}
+
+Write-Host "Done renaming files"
