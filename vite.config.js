0f2a7d6126a5cdefe68b5d44642a269943ca1085
Signature Dubai <wzm2383983461@163.com>
2026-01-29 14:25:18 +0800

    Fix hero overlay and image paths

--- vite.config.js
+++ vite.config.js
@@ -98,7 +98,7 @@ export default defineConfig({
           dest: 'aboutus/team-awards'
         },
         {
-          src: 'public/aboutus/*.png',
+          src: 'public/aboutus/*.*',
           dest: 'aboutus'
         },
         {
